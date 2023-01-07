7df7570ab19133bfe30edf07930c92706a889079
Plato78 <114007402+Plato78@users.noreply.github.com>
2023-01-07 18:59:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Graphics.hpp>
+#include "Function.h"
+#include "settings.h"
+#include "BAT.h"
+#include "BALL.h"
+using namespace sf;
+
+int main()
+{
+	// Îáúåêò, êîòîðûé, ñîáñòâåííî, ÿâëÿåòñÿ ãëàâíûì îêíîì ïðèëîæåíèÿ
+	RenderWindow window(VideoMode(width, height), title);
+	Bat bat;
+	batInit(bat);
+	//ШАР
+	Ball ball;
+	ballInit(ball);
+	// Ãëàâíûé öèêë ïðèëîæåíèÿ. Âûïîëíÿåòñÿ, ïîêà îòêðûòî îêíî
+	while (window.isOpen()) {
+		checkEvents(window);
+		updateGame(bat, ball);
+		checkCollision();
+		drawGame(window, bat, ball);
+	}
+	return 0;
+}
